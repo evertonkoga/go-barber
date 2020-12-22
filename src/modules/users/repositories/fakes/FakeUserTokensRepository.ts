3cdbcca86f47086522603d6b4e68b55e5427472c
Everton Koga <everton.koga@sotran.com.br>
2020-12-22 07:56:04 -0300

    criado testes para reset de senha

--- src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
+++ src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -5,18 +5,26 @@ import UserToken from "@modules/users/infra/typeorm/entities/UserToken";
 
 class FakeUserTokensRepository implements IUserTokensRepository {
 
-  private userToken: UserToken[] = [];
+  private userTokens: UserToken[] = [];
 
-  public async generate(user_id: string): Promise<UserToken | undefined> {
+  public async generate(user_id: string): Promise<UserToken> {
     const userToken = new UserToken();
 
     Object.assign(userToken, {
       id: uuid_v4(),
       token: uuid_v4(),
       user_id,
+      created_at: new Date(),
+      updated_at: new Date(),
     });
 
-    this.userToken.push(userToken);
+    this.userTokens.push(userToken);
+
+    return userToken;
+  }
+
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = this.userTokens.find(findToken => findToken.token === token);
 
     return userToken;
   }
