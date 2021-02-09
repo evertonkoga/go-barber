5d00e62dda7b59199719dad25a46724b7238c280
Everton Koga <everton.koga@sotran.com.br>
2021-02-09 06:53:47 -0300

    listar agendamentos proprios

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -2,12 +2,15 @@ import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import AppointmentsController from '../controllers/AppointmentsController';
+import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController';
 
 const appointmentsRoutes = Router();
 const appointmentsController = new AppointmentsController();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentsRoutes.use(ensureAuthenticated);
 
 appointmentsRoutes.post('/', appointmentsController.create);
+appointmentsRoutes.get('/me', providerAppointmentsController.index);
 
 export default appointmentsRoutes;
