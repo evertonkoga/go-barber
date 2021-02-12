15f0131643e9561354ed2b3769c951d0729c7934
Everton Koga <everton.koga@sotran.com.br>
2021-02-12 07:57:20 -0300

    implementado rate limiter flexible para segurança

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -7,6 +7,7 @@ import { errors } from 'celebrate';
 import 'express-async-errors';
 
 import routes from '@shared/infra/http/routes';
+import rateLimiter from './middlewares/rateLimiter';
 
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
@@ -16,6 +17,7 @@ import '@shared/container';
 
 const app = express();
 
+app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadFolder));
