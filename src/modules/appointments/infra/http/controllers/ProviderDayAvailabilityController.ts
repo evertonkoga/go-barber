5a61f36ebdf46c3e5d419eb6284c85f1bf9cd718
Everton Koga <everton.koga@sotran.com.br>
2021-02-13 16:44:55 -0300

    ajuste na logica

--- src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
+++ src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
@@ -6,12 +6,12 @@ import ListProviderDayAvailabilityService from '@modules/appointments/services/L
 export default class ProviderDayAvailabilityController {
   public async index(request: Request, response: Response): Promise<Response> {
     const { provider_id } = request.params;
-    const { day, month, year } = request.body;
+    const { day, month, year } = request.query;
 
     const listProviderDayAvailability = container.resolve(ListProviderDayAvailabilityService);
 
     const availability = await listProviderDayAvailability.execute({
-      provider_id, day, month, year,
+      provider_id, day: Number(day), month: Number(month), year: Number(year),
     });
 
     return response.json(availability);
